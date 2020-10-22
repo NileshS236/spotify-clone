2c62f84afbdf3fb38419183e11817156cd120003
NileshS236 <nileshsamota0602@gmail.com>
2020-10-23 01:56:13 +0530

    commit

--- src/spotify.js
+++ src/spotify.js
@@ -1,17 +1,13 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "http://localhost:3000/";
 const clientId = "47e74577458d44568ab4feb155cf8bb5";
-
+const redirectUri = "http://localhost:3000/";
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
   "user-read-playback-state",
   "user-top-read",
   "user-modify-playback-state",
-  "streaming",
-  "user-read-email",
-  "user-read-private",
 ];
 
 export const getTokenFromUrl = () => {
@@ -19,8 +15,9 @@ export const getTokenFromUrl = () => {
     .substring(1)
     .split("&")
     .reduce((initial, item) => {
-      let parts = item.split("=");
+      var parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
+
       return initial;
     }, {});
 };
