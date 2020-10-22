3e7121b3ae2ae01af55e84529517f17db0266b87
NileshS236 <nileshsamota0602@gmail.com>
2020-10-23 01:21:19 +0530

    commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAD8mXpyTRwZcJ6DaQH1yYD-2MMSwjGLSM",
+  authDomain: "spotify-clone-8b1af.firebaseapp.com",
+  databaseURL: "https://spotify-clone-8b1af.firebaseio.com",
+  projectId: "spotify-clone-8b1af",
+  storageBucket: "spotify-clone-8b1af.appspot.com",
+  messagingSenderId: "87375522037",
+  appId: "1:87375522037:web:80cab9c488e10b4c0cfdbc",
+  measurementId: "G-TJ6EGKLM3X",
+};
